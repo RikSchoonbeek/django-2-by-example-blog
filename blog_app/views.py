96a1c9c99d6425be4501ec58d1c50e3c904b2122
rikschoonbeek <rik.schoonbeek@gmail.com>
2019-09-04 18:37:22 +0200

    Added class based view

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,25 +2,15 @@ from django.core.paginator import (
     Paginator, EmptyPage, PageNotAnInteger
 )
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an	integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog_app/post/list.html',
-                  {'page':	page,
-                   'posts':	posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'  # 'object_list' by default
+    paginate_by = 3
+    template_name = 'blog_app/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
